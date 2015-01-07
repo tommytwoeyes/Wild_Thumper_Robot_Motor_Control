271f9c1d6cfe13b1daa844f386b43eff8f725849
Tom Malone <tmalone+git@gmail.com>
2015-01-06 20:23:17 -0500

    Added code for blinnking an LED, to help troubleshoot (i.e. if the LED blinks, at least that much of the code is known to work).

--- motor_test.c
+++ motor_test.c
@@ -13,13 +13,29 @@
 #include <util/delay.h>
 #include "motor_control.h"
 
+#define LED         PB0
+#define LED_DDR     DDRB
+#define LED_PORT    PORTB
+#define DELAYTIME   5000
+
 int main(void)
 {
 	// Initialization
 	initializePWM();
 	initializeMotors();
 
+    // Configure LED pin for ouput in LED Data Direction Register
+    LED_DDR |= (1 << LED);
+
 	while (1) {
-		goForward(150);
+        // Turn on LED
+        LED_PORT |= (1 << LED);
+        _delay_ms(DELAYTIME);
+
+        // Turn off LED
+        LED_PORT &= ~(1 << LED);
+        _delay_ms(DELAYTIME);
+
+		motorGo(RIGHT_MOTOR, CW, 50);
 	}
 }
